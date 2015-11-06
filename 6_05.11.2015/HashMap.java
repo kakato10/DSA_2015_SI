19229c09187a8a9746f739b20d0a5cb0a45a5165
U-DESKTOP-G3OEO3N\kakato10 <svetimir.ignatov@gmail.com>
2015-11-06 11:49:59 +0200

    Making the code a bit cleamer and adding shrinking of the HashMap

--- 6_05.11.2015/HashMap.java
+++ 6_05.11.2015/HashMap.java
@@ -2,11 +2,14 @@ public class HashMap {
     private Entry[] table;
     private static final int INITIAL_SIZE = 4;
     private static final int GROWING_FACTOR = 2;
+    private static final int SHRINKING_FACTOR = 2;
+
     private static final double MAX_LOAD_FACTOR = 0.75;
+    private static final double MIN_LOAD_FACTOR = 0.25;
     private int elementsCount;
 
     private int hash(String key) {
-        int hash = 0;
+        int hash = 21;
         for (int i = 0; i < key.length(); i++) {
             char currentChar = key.charAt(i);
             hash += currentChar + i * currentChar;
@@ -15,16 +18,39 @@ public class HashMap {
     }
 
     private int indexer(int hash) {
-        return hash % table.length;
+        return hash % this.table.length;
     }
 
     private Entry[] table() {
         return this.table;
     }
 
-    private HashMap(int size) {
-        this.table = new Entry[size];
-        this.elementsCount = 0;
+    private void swapTables(HashMap hash) {
+        for (Entry head : this.table) {
+            while (head != null) {
+                hash.add(head.key(), head.value());
+                head = head.next();
+            }
+        }
+        this.table = hash.table();
+    }
+
+    private void tryGrow() {
+        double loadFactor = (double) this.elementsCount / this.table.length;
+        if (loadFactor >= MAX_LOAD_FACTOR) {
+            HashMap hash = new HashMap(this.table.length * GROWING_FACTOR);
+            swapTables(hash);
+            System.out.println("GROWING to: " + this.table.length);
+        }
+    }
+
+    private void tryShrink() {
+        double loadFactor = this.elementsCount / this.table.length;
+        if (loadFactor <= MIN_LOAD_FACTOR) {
+            HashMap hash = new HashMap(this.table.length / SHRINKING_FACTOR);
+            swapTables(hash);
+            System.out.println("SHRINKING to: " + this.table.length);
+        }
     }
 
     public HashMap() {
@@ -32,64 +58,58 @@ public class HashMap {
         this.elementsCount = 0;
     }
 
+    public HashMap(int size) {
+        this.table = new Entry[size];
+        this.elementsCount = 0;
+    }
+
+
     public void add(String key, int value) {
-        int index= this.indexer(this.hash(key));
+        int hash = this.hash(key);
+        int index = this.indexer(hash);
         Entry entry = new Entry(key, value);
-        //check for collision
-        if (table[index] == null) {
+        if (this.table[index] == null)
             this.table[index] = entry;
-        } else {
-            System.out.println("Collision!!!" + key);
+        else {
             Entry head = this.table[index];
-            if (head.key().equals(key)) {
-                head.setValue(value);
-                return;
-            }
             while (head.next() != null) {
+                if (head.key().equals(key)) {
+                    head.setValue(value);
+                    return;
+                }
                 head = head.next();
             }
             head.setNext(entry);
         }
-        this.elementsCount++;
-
-        //check if the table should grow
-        double loadFactor = (double)this.elementsCount / this.table.length;
-        if (loadFactor >= MAX_LOAD_FACTOR) {
-            System.out.println("Copping!");
-            HashMap composeTable = new HashMap(this.table.length * GROWING_FACTOR);
-            for (int i = 0; i < this.table.length; i++) {
-                Entry head = this.table[i];
-                while (head != null) {
-                    composeTable.add(head.key(), head.value());
-                    head = head.next();
-                }
-            }
-            this.table = composeTable.table();
-            System.out.println("Table just got bigger " + this.table.length);
-        }
+        elementsCount++;
+        this.tryGrow();
     }
 
     public int get(String key) {
-        int index = this.indexer(this.hash(key));
-        int result = -1;
+        int hash = this.hash(key);
+        int index = this.indexer(hash);
+        int value = -1;
         Entry head = this.table[index];
         while (head != null) {
             if (head.key().equals(key)) {
-                result = head.value();
+                value = head.value();
                 break;
-            } else {
-                head = head.next();
             }
+            head = head.next();
         }
-        return result;
+        return value;
     }
 
     public boolean remove(String key) {
-        int index = this.indexer(this.hash(key));
-        Entry head = this.table[index];
         boolean removed = false;
+        int hash = this.hash(key);
+        int index = this.indexer(hash);
+        Entry head = this.table[index];
         if (head.key().equals(key)) {
-            this.table[index] = null;
+            if (head.next() != null)
+                this.table[index] = head.next();
+            else
+                this.table[index] = null;
             removed = true;
             elementsCount--;
         } else {
@@ -99,24 +119,21 @@ public class HashMap {
                     removed = true;
                     elementsCount--;
                     break;
-                }
-                else {
+                } else {
                     head = head.next();
                 }
             }
         }
+        tryShrink();
         return removed;
     }
 
     public void print() {
         for (Entry head : this.table) {
-            if (head != null) {
-                while (head != null) {
-                    System.out.println(head.toString());
-                    head = head.next();
-                }
+            while (head != null) {
+                System.out.println(head.toString());
+                head = head.next();
             }
         }
     }
-
 }
